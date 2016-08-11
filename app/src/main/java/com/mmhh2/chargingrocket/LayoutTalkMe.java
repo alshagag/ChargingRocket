d82decca6c8052cb402175cb885217bb6e9682fe
abu3li <hossainalyousif@gmail.com>
2016-08-11 19:46:27 +0300

    تعديلات واضافات

--- app/src/main/java/com/mmhh2/chargingrocket/LayoutTalkMe.java
+++ app/src/main/java/com/mmhh2/chargingrocket/LayoutTalkMe.java
@@ -30,7 +30,11 @@ public class LayoutTalkMe extends Fragment {
      * The fragment argument representing the section number for this
      * fragment.
      */
-    private String  numID, type;
+    protected EditText EdTNumPhone;
+    protected Button BuTalkMe;
+    private String  numID, type,number;
+    private String numPhone;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE_TalkMe = 1;
 
 
     public LayoutTalkMe() {
@@ -53,9 +57,36 @@ public class LayoutTalkMe extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.layout_talk_me, container, false);
+        EdTNumPhone = (EditText) rootView.findViewById(R.id.EdTNumPhone);
+        BuTalkMe = (Button) rootView.findViewById(R.id.BuTalkMe);
+        final Context context = getActivity();
 
+        BuTalkMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadData();
+                numPhone = EdTNumPhone.getText().toString();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        if (!shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)) {
+                            requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST_CALL_PHONE_TalkMe);
+                        }
+
+                        return;
+                    }
+                }
+                number = setVariablesTalkMe(context);
+                if (checkTalkMe(context)) {
+                    startActivity(call(number));
+                }
+            }
+
+
+        });
         return rootView;
     }
+
+
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
 
@@ -76,5 +107,82 @@ public class LayoutTalkMe extends Fragment {
 
 
     }
+    protected Intent call(String number) {
+        Intent i = new Intent(Intent.ACTION_CALL, Uri.fromParts("tel", number, null));
+        return i;
+    }
+    public String setVariablesTalkMe(Context context) {
+        int typeNumberTalkMe;
+
+        try {
+
+            if (type.equals("stc")) {
+                typeNumberTalkMe = 177;
+                return "*" + typeNumberTalkMe + "*" + numPhone + "*" + "#";
+            } else if (type.equals("mobily")) {
+                typeNumberTalkMe = 199;
+                return "*" + typeNumberTalkMe + "*" + numPhone + "*" + "#";
+            } else if (type.equals("zain")) {
+                typeNumberTalkMe = 123;
+                return "*" + typeNumberTalkMe + "*" + numPhone + "*" + "#";
+            } else {
+                return "none";
+            }
+
+        } catch (Exception ex) {
+            Toast.makeText(context, getResources().getString(R.string.noneNetwork), Toast.LENGTH_SHORT).show();
+
+        }
+        return "none";
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        Context context = getActivity();
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CALL_PHONE_TalkMe: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    number = setVariablesTalkMe(context);
+                    if (checkTalkMe(context)) {
+                        startActivity(call(number));
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                break;
+            }
+
+
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public boolean checkTalkMe(Context context) {
+        try {
+
+
+            if (!type.equals("none")) {
+                return true;
+            }
+            else {
+                Toast.makeText(context, getResources().getString(R.string.noneNetwork), Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception ex) {
+            Toast.makeText(context, getResources().getString(R.string.noneNetwork), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return false;
+    }
+
+
+
+
 }
 
--- app/src/main/java/com/mmhh2/chargingrocket/MainFragment.java
+++ app/src/main/java/com/mmhh2/chargingrocket/MainFragment.java
@@ -97,6 +97,7 @@ public class MainFragment extends Fragment {
                     FragmentTransaction transaction = manager.beginTransaction();
                     transaction.replace(R.id.LiL,new LayoutCharge(),"Charge");
                     transaction.commit();
+
                 }
 
 
@@ -224,6 +225,27 @@ public class MainFragment extends Fragment {
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.replace(R.id.LiL,new LayoutCharge(),"Charge");
         transaction.commit();
+    }
+    public static ViewGroup getParent(View view) {
+        return (ViewGroup)view.getParent();
+    }
+
+    public static void removeView(View view) {
+        ViewGroup parent = getParent(view);
+        if(parent != null) {
+            parent.removeView(view);
+        }
+    }
+
+    public static void replaceView(View currentView, View newView) {
+        ViewGroup parent = getParent(currentView);
+        if(parent == null) {
+            return;
+        }
+        final int index = parent.indexOfChild(currentView);
+        removeView(currentView);
+        removeView(newView);
+        parent.addView(newView, index);
     }
 }
 
